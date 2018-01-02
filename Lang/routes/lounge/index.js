29b4fafd38d6e9ff4ef10dbca10aada464c1e82f
nohjunho <njh910505@gmail.com>
2018-01-03 00:30:27 +0900

    18) 라운지 피드 작성 api  완료

--- Lang/routes/lounge/index.js
+++ Lang/routes/lounge/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const loungePid = require('./loungePid');
 const loungeLike = require('./loungeLike');
 const loungeDetail = require('./loungeDetail');
+const loungePosting = require('./loungePosting');
 
 router.use('/loungePid', loungePid);
 router.use('/loungeLike', loungeLike);
 router.use('/loungeDetail', loungeDetail);
+router.use('/loungePosting', loungePosting);
 
 module.exports = router;
