8b581473b2a5143e432b3c6bb08cb8f96d7d18c7
LG\USER <doll0462@naver.com>
2018-01-04 11:56:45 +0900

    마이페이지 20. 내 페이지 /  21.남 페이지 22. 내 모임 / 23. 내 라운지 / 22-2. 남 모임(승인여부x) 24. 팔로워 리스트 / 25. 팔로잉 리스트 27. 프로필 수정

--- Lang/routes/index.js
+++ Lang/routes/index.js
@@ -1,22 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
-const user = require('./user');
-router.use('/user', user);
-
-const meeting = require('./meeting');
-router.use('/meeting', meeting);
-
-const lounge = require('./lounge');
-router.use('/lounge', lounge);
-
-const chatting = require('./chatting');
-router.use('/chatting', chatting);
+// const user = require('./user');
+// router.use('/user', user);
+//
+// const meeting = require('./meeting');
+// router.use('/meeting', meeting);
+//
+// const lounge = require('./lounge');
+// router.use('/lounge', lounge);
+//
+// const chatting = require('./chatting');
+// router.use('/chatting', chatting);
 
 const mypage = require('./mypage');
 router.use('/mypage', mypage);
 
-const notification = require('./notification');
-router.use('/notification', notification);
+// const notification = require('./notification');
+// router.use('/notification', notification);
 
 module.exports = router;
